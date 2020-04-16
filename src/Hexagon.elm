41e0dae918d3e4d609c8e5c910550e461b3d2a25
David North <hobnob@users.noreply.github.com>
2020-04-16 22:07:30 +0100

    Adds Hexagon rotation and supporting tests

---
+++ src/Hexagon.elm
@@ -0,0 +1,74 @@
+module Hexagon exposing (rotate)
+
+import Bitwise exposing (and)
+
+
+rotate : ( Int, Int ) -> ( Int, Int ) -> Int -> ( Int, Int )
+rotate origin rotationPoint numTurns =
+    let
+        rotateResult =
+            singleRotate origin rotationPoint
+    in
+    case numTurns of
+        0 ->
+            origin
+
+        1 ->
+            rotateResult
+
+        other ->
+            if other < 0 then
+                rotate rotateResult rotationPoint (other + 1)
+
+            else
+                rotate rotateResult rotationPoint (other - 1)
+
+
+singleRotate : ( Int, Int ) -> ( Int, Int ) -> ( Int, Int )
+singleRotate origin rotationPoint =
+    let
+        ( originX, originY, originZ ) =
+            oddRowToCube origin
+
+        ( rotateX, rotateY, rotateZ ) =
+            oddRowToCube rotationPoint
+
+        ( rX, rY, rZ ) =
+            ( -(originZ - rotateZ), -(originX - rotateX), -(originY - rotateY) )
+    in
+    cubeToOddRow
+        ( rX + rotateX, rY + rotateY, rZ + rotateZ )
+
+
+cubeToOddRow : ( Int, Int, Int ) -> ( Int, Int )
+cubeToOddRow ( x, _, z ) =
+    ( x + (z - Bitwise.and z 1) // 2
+    , z
+    )
+
+
+oddRowToCube : ( Int, Int ) -> ( Int, Int, Int )
+oddRowToCube ( x, y ) =
+    let
+        newX =
+            x - (y - Bitwise.and y 1) // 2
+    in
+    ( newX
+    , -newX - y
+    , y
+    )
+
+
+
+{-
+   function cube_to_oddr(cube):
+          var col = cube.x + (cube.z - (cube.z&1)) / 2
+          var row = cube.z
+          return OffsetCoord(col, row)
+
+      function oddr_to_cube(hex):
+          var x = hex.col - (hex.row - (hex.row&1)) / 2
+          var z = hex.row
+          var y = -x-z
+          return Cube(x, y, z)
+-}
