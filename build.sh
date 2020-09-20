eedc34c9e2a4c6b9097620e0c8aa451f922437d2
David North <hobnob@users.noreply.github.com>
2020-09-20 09:30:02 +0100

    Fixes build scripts for OSX

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
+if [ ! -d build ]; then
+    mkdir build
+fi
 dotnet publish -c Release --self-contained true -r osx-x64 VirtualGloomhavenBoard
+cp VirtualGloomhavenBoard/bin/Release/netcoreapp3.1/osx-x64/publish/VirtualGloomhavenBoard build/vgb-osx-x64
