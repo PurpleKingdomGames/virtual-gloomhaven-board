34187c6c74be5f108272ad9b2c286490b93a55f3
David North <hobnob@users.noreply.github.com>
2020-09-20 15:30:48 +0100

    Adds support for Windows builds

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
 dotnet publish -c Release --self-contained true -r win-x64  VirtualGloomhavenBoard
+copy VirtualGloomhavenBoard\bin\Release\netcoreapp3.1\win-x64\publish\VirtualGloomhavenBoard.exe build\vgb-win-x64
+
+dotnet publish -c Release --self-contained true -r win-x86  VirtualGloomhavenBoard
+copy VirtualGloomhavenBoard\bin\Release\netcoreapp3.1\win-x86\publish\VirtualGloomhavenBoard.exe build\vgb-win-x86
