e0578c0ff2078175b2a49a227ba2f1f61d4e295a
David North <hobnob@users.noreply.github.com>
2020-09-20 16:15:44 +0100

    Drops x86 build from linux as the RID is invalid

--- build-linux.sh
+++ build-linux.sh
@@ -3,6 +3,3 @@ if [ ! -d build ]; then
 fi
 dotnet publish -c Release --self-contained true -r linux-x64 VirtualGloomhavenBoard
 cp VirtualGloomhavenBoard/bin/Release/netcoreapp3.1/linux-x64/publish/VirtualGloomhavenBoard build/vgb-linux-x64
-
-dotnet publish -c Release --self-contained true -r linux-x86 VirtualGloomhavenBoard
-cp VirtualGloomhavenBoard/bin/Release/netcoreapp3.1/linux-x86/publish/VirtualGloomhavenBoard build/vgb-linux-x86
