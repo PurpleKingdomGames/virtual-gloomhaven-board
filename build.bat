52aeb0f6de415e38154b8341ec0492012f684142
David North <hobnob@users.noreply.github.com>
2020-09-22 08:06:38 +0100

    Windows builds weren't being renamed correctly

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 dotnet publish -c Release --self-contained true -r win-x64  VirtualGloomhavenBoard
-copy VirtualGloomhavenBoard\bin\Release\netcoreapp3.1\win-x64\publish\VirtualGloomhavenBoard.exe build\vgb-win-x64
+copy VirtualGloomhavenBoard\bin\Release\netcoreapp3.1\win-x64\publish\VirtualGloomhavenBoard.exe build\vgb-win-x64.exe
 
 dotnet publish -c Release --self-contained true -r win-x86  VirtualGloomhavenBoard
-copy VirtualGloomhavenBoard\bin\Release\netcoreapp3.1\win-x86\publish\VirtualGloomhavenBoard.exe build\vgb-win-x86
+copy VirtualGloomhavenBoard\bin\Release\netcoreapp3.1\win-x86\publish\VirtualGloomhavenBoard.exe build\vgb-win-x86.exe
