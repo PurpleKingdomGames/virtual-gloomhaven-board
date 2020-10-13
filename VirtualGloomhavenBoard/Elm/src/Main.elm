PurpleKingdomGames/virtual-gloomhaven-board

commit e477a7083a68b5d7371a26900f9f4e608a442b8f

Fixes random closing of connection settings
Looks likethe stop propogation may not have been stopping properly. Preventing default as well seems to fix this issue

Fixes #38

VirtualGloomhavenBoard/Elm/src/Main.elm
@@ -843,7 +843,7 @@ getDialogForAppMode model =
                         AppStorage.Game ->
                             \e -> e
                    )
-                |> Dom.addActionStopPropagation ( "click", NoOp )
+                |> Dom.addActionStopAndPrevent ( "click", NoOp )
             )
         |> Dom.addAction ( "click", ChangeAppMode AppStorage.Game )
         |> Dom.render

commit 042a59a162aecab465b03b755b5c16da2ed6afeb

Hotfix - player choice dialog was broken by e477a70

VirtualGloomhavenBoard/Elm/src/Main.elm
@@ -101,7 +101,7 @@ type Msg
 
 version : String
 version =
-    "1.2.0"
+    "1.2.1"
 
 
 undoLimit : Int
@@ -942,7 +942,7 @@ getDialogForAppMode model =
                         AppStorage.Game ->
                             \e -> e
                    )
-                |> Dom.addActionStopAndPrevent ( "click", NoOp )
+                |> Dom.addActionStopPropagation ( "click", NoOp )
             )
         |> Dom.addAction ( "click", ChangeAppMode AppStorage.Game )
         |> Dom.render
@@ -1080,7 +1080,7 @@ getPlayerChoiceDialog model =
                 |> Dom.appendChildList
                     [ Dom.element "button"
                         |> Dom.appendText "OK"
-                        |> Dom.addAction ( "click", ChangePlayerList )
+                        |> Dom.addActionStopPropagation ( "click", ChangePlayerList )
                     , Dom.element "button"
                         |> Dom.appendText "Cancel"
                         |> Dom.addAction ( "click", ChangeAppMode AppStorage.Game )