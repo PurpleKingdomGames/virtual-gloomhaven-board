a6fff2081d17c3089a7f103f9f45f097f142415b
David North <hobnob@users.noreply.github.com>
2020-11-15 19:31:40 +0000

    Fixes build commands to use .Net 5.0

--- build-linux.sh
+++ build-linux.sh
@@ -2,4 +2,4 @@ if [ ! -d build ]; then
     mkdir build
 fi
 dotnet publish -c Release --self-contained true -r linux-x64 VirtualGloomhavenBoard
-cp VirtualGloomhavenBoard/bin/Release/netcoreapp3.1/linux-x64/publish/VirtualGloomhavenBoard build/vgb-linux-x64
+cp VirtualGloomhavenBoard/bin/Release/net5.0/linux-x64/publish/VirtualGloomhavenBoard build/vgb-linux-x64
--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ if [ ! -d build ]; then
     mkdir build
 fi
 dotnet publish -c Release --self-contained true -r osx-x64 VirtualGloomhavenBoard
-cp VirtualGloomhavenBoard/bin/Release/netcoreapp3.1/osx-x64/publish/VirtualGloomhavenBoard build/vgb-osx-x64
+cp VirtualGloomhavenBoard/bin/Release/net5.0/osx-x64/publish/VirtualGloomhavenBoard build/vgb-osx-x64
