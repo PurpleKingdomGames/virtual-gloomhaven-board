54206f5f4a2689712679cfb26de882192f5b31c8
Dave Smith <davesmith.00000@gmail.com>
2023-04-10 10:55:49 +0100

    Initial function ported, does not compile

--- ui/ui/src/vgb/Model.scala
+++ ui/ui/src/vgb/Model.scala
@@ -1,6 +1,8 @@
 package vgb
 
 import org.scalajs.dom.Element
+import cats.effect.IO
+import tyrian.*
 
 final case class Model(
     game: Game,
@@ -33,174 +35,162 @@ final case class Model(
 
 object Model:
 
-  init : ( Maybe Decode.Value, Maybe Decode.Value, Int ) -> ( Model, Cmd Msg )
-  init ( oldState, maybeOverrides, seed ) =
-      let
-          overrides =
-              case maybeOverrides of
-                  Just o ->
-                      case loadOverrides o of
-                          Ok ov ->
-                              ov
+  def init(oldState: Option[String], maybeOverrides: Option[String], seed: Int): (Model, Cmd[IO, Msg]) = {
+    val overrides =
+      maybeOverrides match
+        case Some(o) =>
+          loadOverrides(o) match
+            case Ok(ov) =>
+              ov
 
-                          _ ->
-                              emptyOverrides
+            case _ =>
+              emptyOverrides
 
-                  Nothing ->
-                      emptyOverrides
+        case None =>
+          emptyOverrides
 
-          ( gs, initConfig ) =
-              case oldState of
-                  Just s ->
-                      case loadFromStorage s of
-                          Ok ( g, c ) ->
-                              ( g
-                              , { c
-                                  | roomCode =
-                                      case overrides.initRoomCodeSeed of
-                                          Just _ ->
-                                              Nothing
+    val (gs, initConfig) =
+      oldState match
+        case Some(s) =>
+          loadFromStorage(s) match
+            case Ok((g, c)) =>
+              (
+                g,
+                c.copy(
+                  roomCode = overrides.initRoomCodeSeed match {
+                    case Some(_) =>
+                      Nothing
 
-                                          Nothing ->
-                                              c.roomCode
-                                  , campaignTracker =
-                                      case overrides.campaignTracker of
-                                          Just t ->
-                                              Just t
+                    case None =>
+                      c.roomCode
+                  },
+                  campaignTracker = overrides.campaignTracker match {
+                    case Some(t) =>
+                      Option(t)
 
-                                          Nothing ->
-                                              c.campaignTracker
-                                }
-                              )
-
-                          Err _ ->
-                              AppStorage.empty
-
-                  Nothing ->
-                      AppStorage.empty
-
-          forceScenarioRefresh =
-              (case overrides.initPlayers of
-                  Nothing ->
-                      List.length gs.players == 0
-
-                  Just p ->
-                      (List.length p > 0 && p /= gs.players)
-                          || (List.length gs.players == 0)
-              )
-                  || (case overrides.initScenario of
-                          Just newScenario ->
-                              case gs.scenario of
-                                  InbuiltScenario Gloomhaven oldScenario ->
-                                      newScenario /= oldScenario
-
-                                  _ ->
-                                      True
-
-                          Nothing ->
-                              False
-                     )
-
-          initGameState =
-              { gs
-                  | updateCount = 0
-                  , players =
-                      case overrides.initPlayers of
-                          Just p ->
-                              if List.length p > 0 then
-                                  p
-
-                              else
-                                  gs.players
-
-                          Nothing ->
-                              gs.players
-                  , roomCode =
-                      case overrides.initRoomCodeSeed of
-                          Just _ ->
-                              ""
-
-                          Nothing ->
-                              gs.roomCode
-              }
-
-          initGame =
-              Game.empty
-
-          initScenario =
-              case overrides.initScenario of
-                  Just i ->
-                      InbuiltScenario Gloomhaven i
-
-                  Nothing ->
-                      initGameState.scenario
-
-          model =
-              Model
-                  { initGame | state = initGameState }
-                  initConfig
-                  (Loading initGameState.scenario)
-                  Nothing
-                  Nothing
-                  Nothing
-                  Nothing
-                  Nothing
-                  Nothing
-                  (getDeadPlayers initGameState)
-                  Nothing
-                  Disconnected
-                  []
-                  False
-                  False
-                  False
-                  overrides.lockScenario
-                  overrides.lockPlayers
-                  overrides.lockRoomCode
-                  overrides.initRoomCodeSeed
-                  []
-                  True
-                  Nothing
-                  Closed
-                  ( 0, 0 )
-                  ( 0, 0 )
-      in
-      case initScenario of
-          InbuiltScenario _ _ ->
-              ( model
-              , Cmd.batch
-                  [ connectToServer
-                  , loadScenarioById
-                      initScenario
-                      (LoadedScenarioHttp
-                          (Random.initialSeed seed)
-                          initScenario
-                          (if forceScenarioRefresh then
-                              Nothing
-
-                           else
-                              Just initGameState
-                          )
-                          False
-                      )
-                  ]
+                    case None =>
+                      c.campaignTracker
+                  }
+                )
               )
 
-          CustomScenario s ->
-              let
-                  ( m, msg ) =
-                      update
-                          (LoadedScenarioJson
-                              (Random.initialSeed seed)
-                              initScenario
-                              (if forceScenarioRefresh then
-                                  Nothing
+            case Err(_) =>
+              AppStorage.empty
 
-                               else
-                                  Just initGameState
-                              )
-                              False
-                              (decodeString decodeScenario s)
-                          )
-                          model
-              in
-              ( m, Cmd.batch [ connectToServer, msg ] )
+        case None =>
+          AppStorage.empty
+
+    val forceScenarioRefresh =
+      (overrides.initPlayers match
+        case None =>
+          gs.players.length == 0
+
+        case Some(p) =>
+          (p.length > 0 && p /= gs.players) || (gs.players.length == 0)
+      )
+        || (overrides.initScenario match
+          case Some(newScenario) =>
+            gs.scenario match
+              case InbuiltScenario(Gloomhaven, oldScenario) =>
+                newScenario /= oldScenario
+
+              case _ =>
+                true
+
+          case None =>
+            false
+        )
+
+    val initGameState =
+      gs.copy(
+        updateCount = 0,
+        players = overrides.initPlayers match
+          case Some(p) =>
+            if p.length > 0 then p else gs.players
+
+          case None =>
+            gs.players
+        ,
+        roomCode = overrides.initRoomCodeSeed match
+          case Some(_) =>
+            ""
+
+          case None =>
+            gs.roomCode
+      )
+
+    val initGame =
+      Game.empty
+
+    val initScenario =
+      overrides.initScenario match
+        case Some(i) =>
+          InbuiltScenario(Gloomhaven, i)
+
+        case None =>
+          initGameState.scenario
+
+    val model =
+      Model(
+        initGame.copy(state = initGameState),
+        initConfig,
+        Loading(initGameState.scenario),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        getDeadPlayers(initGameState),
+        Nothing,
+        Disconnected,
+        Nil,
+        false,
+        false,
+        false,
+        overrides.lockScenario,
+        overrides.lockPlayers,
+        overrides.lockRoomCode,
+        overrides.initRoomCodeSeed,
+        Nil,
+        true,
+        None,
+        Closed,
+        (0, 0),
+        (0, 0)
+      )
+
+    initScenario match
+      case InbuiltScenario(_, _) =>
+        (
+          model,
+          Cmd.Batch(
+            connectToServer,
+            loadScenarioById(
+              initScenario,
+              LoadedScenarioHttp(
+                Random.initialSeed(seed),
+                initScenario,
+                if forceScenarioRefresh then None
+                else Option(initGameState),
+                false
+              )
+            )
+          )
+        )
+
+      case CustomScenario(s) =>
+        val (m, msg) =
+          update(
+            LoadedScenarioJson(
+              Random.initialSeed(seed),
+              initScenario,
+              if forceScenarioRefresh then None else Option(initGameState),
+              false,
+              (decodeString decodeScenario s)
+            ),
+            model
+          )
+
+        (m, Cmd.Batch(connectToServer, msg))
+  }
